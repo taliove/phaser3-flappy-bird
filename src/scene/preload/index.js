b58d78b260cc340f316a7cc878a107847d4b2310
tangfan <tangf@gzjunbo.net>
2018-10-22 16:18:31 +0800

    使用build 组织webpack

--- src/scene/preload/index.js
+++ src/scene/preload/index.js
@@ -3,28 +3,28 @@
  */
 export default {
     preload() {
-        this.load.image("loading", "assets/preloader.gif");
-        this.load.image("background", "assets/background.png");
-        this.load.image("ground", "assets/ground.png");
-        this.load.image("title", "assets/title.png");
+        this.load.image("loading", "static/assets/preloader.gif");
+        this.load.image("background", "static/assets/background.png");
+        this.load.image("ground", "static/assets/ground.png");
+        this.load.image("title", "static/assets/title.png");
 
-        this.load.spritesheet("bird", 'assets/bird.png', {
+        this.load.spritesheet("bird", 'static/assets/bird.png', {
             frameWidth: 34, frameHeight: 24
         }); //鸟
-        this.load.image("btn", 'assets/start-button.png');  //按钮
-        this.load.spritesheet("pipe", 'assets/pipes.png', {
+        this.load.image("btn", 'static/assets/start-button.png');  //按钮
+        this.load.spritesheet("pipe", 'static/assets/pipes.png', {
             frameWidth: 54, frameHeight: 320
         }); //管道
-        this.load.bitmapFont("flappy_font", 'assets/fonts/flappyfont/flappyfont.png', 'assets/fonts/flappyfont/flappyfont.fnt');//显示分数的字体
-        this.load.audio("fly_sound", 'assets/flap.wav');//飞翔的音效
-        this.load.audio("score_sound", 'assets/score.wav');//得分的音效
-        this.load.audio("hit_pipe_sound", 'assets/pipe-hit.wav'); //撞击管道的音效
-        this.load.audio("hit_ground_sound", 'assets/ouch.wav'); //撞击地面的音效
+        this.load.bitmapFont("flappy_font", 'static/assets/fonts/flappyfont/flappyfont.png', 'static/assets/fonts/flappyfont/flappyfont.fnt');//显示分数的字体
+        this.load.audio("fly_sound", 'static/assets/flap.wav');//飞翔的音效
+        this.load.audio("score_sound", 'static/assets/score.wav');//得分的音效
+        this.load.audio("hit_pipe_sound", 'static/assets/pipe-hit.wav'); //撞击管道的音效
+        this.load.audio("hit_ground_sound", 'static/assets/ouch.wav'); //撞击地面的音效
 
-        this.load.image('ready_text', 'assets/get-ready.png'); //get ready图片
-        this.load.image('play_tip', 'assets/instructions.png'); //玩法提示图片
-        this.load.image('game_over', 'assets/gameover.png'); //gameover图片
-        this.load.image('score_board', 'assets/scoreboard.png'); //得分板
+        this.load.image('ready_text', 'static/assets/get-ready.png'); //get ready图片
+        this.load.image('play_tip', 'static/assets/instructions.png'); //玩法提示图片
+        this.load.image('game_over', 'static/assets/gameover.png'); //gameover图片
+        this.load.image('score_board', 'static/assets/scoreboard.png'); //得分板
     },
     create() {
         this.bg = this.add.tileSprite(144, 505 / 2, 288, 505, 'background');
